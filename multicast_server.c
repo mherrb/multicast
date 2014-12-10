c0ee1cdc1578a4bf02fb5ac8480f6d5dc952a7bf
Matthieu Herrb <matthieu.herrb@laas.fr>
2014-12-10 16:40:10 +0100

    add -i parameter to control the rate

--- multicast_server.c
+++ multicast_server.c
@@ -21,16 +21,32 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <err.h>
+#include <limits.h>
 #include <netdb.h>
 #include <stdio.h>      /* for fprintf() */
 #include <stdlib.h>     /* for atoi() and exit() */
 #include <string.h>
 #include <unistd.h>
 
+#ifdef HAVE_LIBBSD
+# include <bsd/stdlib.h>	/* for strtonum() */
+#endif
+
+void
+usage(const char *name)
+{
+	errx(2, "Usage: %s [-i interval(ms)] <Multicast Address> <Port> "
+	    "<Send String> [<TTL>]", name);
+}
+
+
 int
 main(int argc, char *argv[])
 {
 	int sock;		      /* Socket */
+	int ch;
+	unsigned long interval = 3000;
+	const char *errmsg;
 	char *multicastIP;            /* Arg: IP Multicast address */
 	char *multicastPort;          /* Arg: Server port */
 	char *sendString;             /* Arg: String to multicast */
@@ -39,18 +55,31 @@ main(int argc, char *argv[])
 	struct addrinfo* multicastAddr;          /* Multicast address */
 	struct addrinfo hints = { 0 }; /* Hints for name lookup */
 
+	while ((ch = getopt(argc, argv, "i:")) != -1) {
+		switch (ch) {
+		case 'i':
+			interval = strtonum(optarg, 0, INT_MAX,
+			    &errmsg);
+			if (errmsg != NULL)
+				err(2, "-i: %s", errmsg);
+			break;
+		default:
+			usage(argv[0]);
+		}
+	}
+	argc -= optind;
+	argv += optind;
 
-	if (argc < 4 || argc > 5)
+	if (argc < 3 || argc > 4)
 		errx(2, "Usage: %s <Multicast Address> <Port> <Send String>"
 		    " [<TTL>]", argv[0]);
 
-	multicastIP = argv[1]; /* First arg:   multicast IP address */
-	multicastPort = argv[2]; /* Second arg:  multicast port */
-	sendString = argv[3];	/* Third arg:   String to multicast */
+	multicastIP = argv[0]; /* First arg:   multicast IP address */
+	multicastPort = argv[1]; /* Second arg:  multicast port */
+	sendString = argv[2];	/* Third arg:   String to multicast */
        /* Fourth arg:  If supplied, use command-line */
 	/* specified TTL, else use default TTL of 1 */
-	multicastTTL = (argc == 5 ? atoi(argv[4]) : 1);
-	sendStringLen = strlen(sendString);
+	multicastTTL = (argc == 4 ? atoi(argv[3]) : 1);
 
 	/* Resolve destination address for multicast datagrams */
 	hints.ai_family   = PF_UNSPEC;
@@ -83,8 +112,8 @@ main(int argc, char *argv[])
 			err(2, "sendto() failed");
 
 
-		usleep(3000000); /* Multicast sendString in datagram to
-				    clients every 3 seconds */
+		usleep(interval*1000); /* Multicast sendString in datagram to
+					  clients every interval ms */
 	}
 
 	/* NOT REACHED */

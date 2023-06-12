d99a693897fa89b844743aa944cd60aa3362f44d
Matthieu Herrb <matthieu@herrb.eu>
2023-06-12 13:52:48 +0200

    gnuplot.cmd: better error handling

--- gnuplot.cmd
+++ gnuplot.cmd
@@ -1,9 +1,9 @@
 #! /usr/bin/env -S gnuplot --persist -c
 
 set key outside
-data = ARG1
-print "arg : ", ARGC
-print "data : ", data
-
-if (!exists("data")) data='data.txt'
+if (ARGC == 0) {
+   data='data.txt'
+} else {
+   data  = ARG1
+}
 plot for [col=1:2] data using 0:col with lines title columnheader
